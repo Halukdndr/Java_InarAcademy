f439727f1930228e943d9a3c6cef2142300266ac
haluk <dundarhaluk05@gmail.com>
2022-09-24 15:04:15 +0300

    Chapter _05

--- src/chapters/chapter_05/exercises/Exercise_22.java
+++ src/chapters/chapter_05/exercises/Exercise_22.java
@@ -1,11 +1,42 @@
 package chapters.chapter_05.exercises;
 
+import java.util.Scanner;
+
 public class Exercise_22 {
 
 	public static void main(String[] args) {
 
-	
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enterloan amount: ");
+		double amount = input.nextDouble();
+		System.out.println("Enter number of years: ");
+		int years = input.nextInt();
+		System.out.println("Enter annual interest rate: ");
+		double annualInterestRate = input.nextDouble();
+		double monthlyInterestRate = annualInterestRate / 1200;
 		
+		double monthlyPayment = (amount * monthlyInterestRate) / (1 - (1 / Math.pow(1 + monthlyInterestRate, years * 12)));
+		double totalPayment = monthlyPayment * years * 12;
+		
+		System.out.printf("\nMonthly Payment: %3.2f" , monthlyPayment);
+		System.out.printf("\nTotal Payment: %7.2f" , totalPayment);
+		System.out.println("\n\nPayment#	Interest	Principal	Balance");
+		
+		double intrest = 0;
+		double principal = 0;
+		double balance = amount;
+		
+		for (int i = 1; i <= years * 12; i++) {
+			intrest = monthlyInterestRate * balance;
+			principal = monthlyPayment - intrest;
+			balance = balance - principal;
+			System.out.printf("%-2d\t\t", i);
+			System.out.printf("%3.2f\t\t", intrest);
+			System.out.printf("%3.2f\t\t", principal);
+			System.out.printf("%7.2f", balance);
+			System.out.println();
+		}
 
 	}
 
--- src/chapters/chapter_05/exercises/Exercise_42.java
+++ src/chapters/chapter_05/exercises/Exercise_42.java
@@ -9,10 +9,9 @@ public class Exercise_42 {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter commission_sought:");
 		double commissionSought = input.nextDouble();
-		double baseSalary = 5_000;
+	
 		double salesAmount = 0;
 		double commission = 0;
-		commissionSought -= baseSalary;
 		for (salesAmount = 0; commission < commissionSought ; salesAmount++ ) {
 			if (salesAmount > 10_000) {
 				commission = (salesAmount - 10_000) * 0.12 + 5_000 * 0.10 + 5_000 * 0.08;
